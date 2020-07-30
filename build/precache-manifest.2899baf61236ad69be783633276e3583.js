56c4e8c49f00b39de9c8c850cd4a1f30c88b7eb2
Gurkaran Singh <grevity.mail@gmail.com>
2020-07-31 02:03:36 +0530

    Doooone!

---
+++ build/precache-manifest.2899baf61236ad69be783633276e3583.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afac374f5fcc8335b346d6237ed5295b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0653d46f4a7dd5a4db1a",
+    "url": "/static/css/main.cb245f8d.chunk.css"
+  },
+  {
+    "revision": "14a6a81acc5ff7e3592d",
+    "url": "/static/js/2.252c720d.chunk.js"
+  },
+  {
+    "revision": "38a32efc2862e82ea90784f450c76373",
+    "url": "/static/js/2.252c720d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0653d46f4a7dd5a4db1a",
+    "url": "/static/js/main.2b4c3e37.chunk.js"
+  },
+  {
+    "revision": "dc192b57bfed2afd6be1ea5558f17255",
+    "url": "/static/js/main.2b4c3e37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efc21b0cf950199eb1b0",
+    "url": "/static/js/runtime-main.a52b60f3.js"
+  },
+  {
+    "revision": "bd6820f1a4015e9645e470ea8e7b1501",
+    "url": "/static/media/anime3.bd6820f1.png"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/static/media/nucleo.f0b489a5.woff"
+  }
+]);
